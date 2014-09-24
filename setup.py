babf900f618a04fecbcb8caa5cadc638a1339178
Evan Myller <eMyller@7ws.co>
2014-09-24 18:47:17 -0300

    Bump to 0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-emailer',
-    version='0.2',
+    version='0.2.1',
     author='Evandro Myller',
     author_email='emyller@7ws.co',
     description='Template based email sending with SMTP connection management',
